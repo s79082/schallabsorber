16dad5bed5e9b4f98ea7903bb7c6b3e47005b071
s79082 <s79082@htw-dresden.de>
2022-05-31 19:31:42 +0200

    plot rt

--- test.py
+++ test.py
@@ -4,108 +4,115 @@ from fft_util import calc_fft as fft
 
 SAMPLERATE = 192000
 
+# amount of frames we split the data
+N_FRAMES = 40
+
 def generate_test_sine():
 
     # gedämpfte 
     nums = np.arange(SAMPLERATE)
     sine1 = np.zeros(SAMPLERATE)
     sine2 = np.zeros(SAMPLERATE)
+    sine3 = np.zeros(SAMPLERATE)
+    sine4 = np.zeros(SAMPLERATE)
+    sine6 = np.zeros(SAMPLERATE)
+    sine7 = np.zeros(SAMPLERATE)
+
     sine1 = np.power(10, -0.0000025 * nums) *  np.sin(0.01 * 2 * np.pi * nums)
-    #sine2 = np.power(10, -0.000045 * nums) *  np.sin(0.005 * 2 * np.pi * nums)
     sine2 = np.power(10, -0.000005 * nums) *  np.sin(0.005 * 2 * np.pi * nums)
+    sine3 = np.power(10, -0.000005 * nums) *  np.sin(0.0025 * 2 * np.pi * nums)
+    sine4 = np.power(10, -0.0000025 * nums) *  np.sin(0.02 * 2 * np.pi * nums)
 
-    final_sine = sine1 + sine2
+    sine5 = np.power(10, -0.000006 * nums) *  np.sin(0.025 * 2 * np.pi * nums)
 
-    #final_sine = np.sin(2 * np.pi * nums)
-    #plt.plot(final_sine)
+    sine6 = np.power(10, -0.000004 * nums) *  np.sin(0.0225 * 2 * np.pi * nums)
+
+    sine7 = np.power(10, -0.0000025 * nums) *  np.sin(0.003 * 2 * np.pi * nums)
+
+    final_sine = sine1 + sine2 + sine3 + sine4 + sine5 + sine6 + sine7
+
     plt.plot(sine1)
     plt.plot(sine2)
+    plt.plot(sine3)
+    plt.plot(sine4)
+    plt.plot(sine5)
+    plt.plot(sine6)
+    plt.plot(sine7)
+    plt.title("einzelne Frequenzen")
     plt.show()
-    return final_sine
 
-def find(data: np.ndarray, n: int = 1) -> list:
-    
-    pass
+    plt.plot(final_sine)
+    plt.title("kombinierte Frequenzen")
+    plt.show()
+
+    return final_sine
 
 data = generate_test_sine()
 
-# show whole fft
+# show spektrum for whole data
 fs, amps = fft(data, SAMPLERATE)
-
 plt.scatter(fs, amps)
 plt.show()
 
-
-# split data
-frame_size = int(SAMPLERATE / 40)
+frame_size = int(SAMPLERATE / N_FRAMES)
+# split data into frames
 frames = [ data[start : start + frame_size] for start in range(0, SAMPLERATE, frame_size) ]
 
 # calc fft for each frame
-fft_data = [ fft(frame, SAMPLERATE) for frame in frames ]
+spectrum_each_frame = [ fft(frame, SAMPLERATE) for frame in frames ]
 
-# measure rt for each frequency
-n_freqs = len(list(filter(lambda x: x >= 0, fft_data[0][0])))
-#max_freqs = np.zeros(n_freqs)
-#wanted_diff = np.zeros(n_freqs)
+# the max map for each freq
 max_amp = dict()
-#wanted_diff = dict()
-wanted_diff = max(fft_data[0][1]) * 0.4
+
+# the wanted difference in amplitude; TODO this should relate to 60dB for RT60
+# now just take 40% of the max amp of the first frame
+wanted_diff = max(spectrum_each_frame[0][1]) * 0.4
+
+# RT in samples for each freq
 rt_for_freq = dict()
 
+for t, (f, a) in enumerate(spectrum_each_frame):
 
-for t, (f, a) in enumerate(fft_data):
+    # cut away negative frequencies and above 5kHz
+    tmp_zip = np.array(list(filter(lambda x: 5000.0 > x[0] >= 0, zip(f, a))))
 
-    # cut away negative frequencies
-    tmp_zip = np.array(list(filter(lambda x: x[0] >= 0, zip(f, a))))
-
-    for i, (freq, amp) in enumerate(tmp_zip):
-
-        if freq == 75000.0:
-        #if freq == 1920 or freq == 960:
-            print(freq, amp)
+    for freq, amp in tmp_zip:
 
         # RT has already been calculated for this frequency
         if max_amp.get(freq) == -1:
             continue
 
-        #if max_freqs[i] == 0:
+        # max amp has not been set for this freq yet
         if not freq in max_amp:
-            # first frame, set amp, assuming its the max
-            #max_freqs[i] = amp
-            #wanted_diff[i] = 0.4 * amp
-            max_amp[freq] = amp
-            #wanted_diff[freq] = 0.4 * amp
 
+            # first frame, set amp, assuming its the max
+            max_amp[freq] = amp
             continue
 
-        #if max_freqs[i] - amp >= wanted_diff[i]:
+        rt_for_freq[freq] = 0
+
+        # the wanted diff (60dB) has been reached
         if max_amp[freq] - amp >= wanted_diff:
 
             # mark freq as done
+            # TODO do this smoother
             max_amp[freq] = -1
 
+            # RT in samples
             samples = t * frame_size
 
-            rt_for_freq[freq] = samples / SAMPLERATE
-            #if samples > 19200:
-                #print("RT for freq {} is {} ({} samples)".format(freq, str(samples / SAMPLERATE), samples))
+            #rt_for_freq[freq] = samples / SAMPLERATE
+            rt_for_freq[freq] = samples
 
-for key, val in rt_for_freq.items():
-    if val is not None:
-        print(key, val)
-print(rt_for_freq[1920.0])
-print(rt_for_freq[960.0])
-print(rt_for_freq.get(1910.0))
+# display rt
+xaxis = np.array(list(rt_for_freq.keys()))
+yaxis = np.array(list(rt_for_freq.values()))
 
-#     sorted_index_array = np.argsort(np.array(list(map(lambda x: x[1], tmp_zip))))
-  
-#     # sorted array
-#     sorted_array = tmp_zip[sorted_index_array]
-#     rslt = sorted_array[-2:]
-#     print(rslt)
-#     plt.scatter(f, a)
-#     plt.show()
+plt.bar(xaxis, yaxis, width=20)
+plt.ylabel("RT60 (samples)")
+plt.xlabel("Frequenz (Hz)")
+plt.title("Nachhallzeit für 192k samples")
+plt.show()
 
-# max_freq_each_split = [ max(zip(fft_res[0], fft_res[1]), key=lambda x: x[1]) for fft_res in fft_data ] 
-
-# print(max_freq_each_split)
+print(xaxis)
+print(yaxis)
