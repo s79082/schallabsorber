380ec67cd0cec18530280db979d67f8606ba82f0
s79082 <s79082@htw-dresden.de>
2022-07-01 15:40:18 +0200

    fixed detector

--- reverberation_detector.py
+++ reverberation_detector.py
@@ -1,42 +1,74 @@
 # script to detect all point of interests in an audiosample
-
-from email.mime import audio
 import numpy as np
+
+def interval_len(interval: tuple[int, int]) -> int:
+    return interval[1] - interval[0]
+
+def generate_intervals(total_lenght: int, n_intervals: int) -> list[tuple[int, int]]:
+    
+    intervals = []
+    len_interval = int(total_lenght / n_intervals)
+
+    for i in range(n_intervals):
+        start = i * len_interval
+        intervals.append((start, start + len_interval))
+
+    return intervals
 
 
 def detect_intervals(data: np.array, l: int = 9) -> list[tuple[int, int]]:
     
+    steps = generate_intervals(len(data), int(len(data) / 100))
+    print(interval_len(steps[0]))
+
     intervals = []
-    duration_samples = 460000.0
+    duration_samples = 430000.0
+    #curr_interval = (0, 0)
+    curr_interval = [0, 0]
 
     # convert wave into db
     dbs = 10 * np.log10(abs(data) / (3.3656 * 10 ** -11))
-    db_thresh = 90.0
-    i = 0
+    db_thresh = 78.0
 
-    while i < len(dbs):
+    for step in steps:
 
-        db = dbs[i]
+        #avg_db_step = 10 * np.log10(np.average(abs(dbs[step[0] : step[1]])) / (3.3656 * 10 ** -11)) 
+        avg_db_step = np.average(dbs[step[0] : step[1]])
+        #avg_db_step = max(dbs[step[0] : step[1]])
+        # print(avg_db_step)
+        if avg_db_step > db_thresh:
+            
+            # set current interval end to end of current step
+            curr_interval[1] = step[1]
 
-        # db over threshold
-        if db > db_thresh:
-            # count section length
-            j = i
-            length = 0
-            while dbs[j] > db_thresh:
-                print(j)
-                length += 1
-                j += 1
-            # section long enough
-            if length > 470000:
-                intervals.append((i, i + length))
-    
-        i += 1
+        elif interval_len(curr_interval) > duration_samples:
+            intervals.append(curr_interval)
+            curr_interval = [step[1], step[1]]
+        else:
+            curr_interval = [step[1], step[1]]
+
 
     return intervals
 
+
+
 if __name__ == "__main__":
     import audiofile
+    import matplotlib.pyplot as plt
     data, _ = audiofile.read("data/wav1.wav")
 
-    print(detect_intervals(data))
+    intervals = detect_intervals(data)
+    print(intervals)
+
+    plt.plot(data)
+
+    starts = [ s[0] for s in intervals ]
+    ends = [ s[1] for s in intervals ]
+
+    plt.vlines(starts, 0, 1, colors=["green"])
+    plt.vlines(ends, 0, 1, colors=["red"])
+
+
+    plt.show()
+    
+
