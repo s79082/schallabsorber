77bef41243fa21d7d40c87527759b5688ab18622
s79082 <s79082@htw-dresden.de>
2022-08-22 17:50:51 +0200

    thresh param

--- tmp.py
+++ tmp.py
@@ -35,7 +35,7 @@ N_FRAMES = 50
 
 def main():
 
-    def f(nfft, nperseg):
+    def f(nfft, nperseg, max_mse, max_slope, thresh):
 
         global txt_nfft, txt_nperseg, axis
 
@@ -46,10 +46,16 @@ def main():
         #nperseg = int(txt_nperseg.get("1.0", tk.END))
         print(nfft, nperseg)
 
-        data, sr = audiofile.read("data/wav1.wav")
-        interval = detect_intervals(data)
-        #interval = [(0, len(data))]
+        #data, sr = audiofile.read("data/wav1.wav")
+        data, sr = audiofile.read("E:/t2-exp.wav")
+        data = data[280000:360000]
+        #plt.plot(data)
+        #plt.show()
+        #interval = detect_intervals(data)
+        interval = [(0, len(data))]
         matrices = []
+
+        SAMPLERATE = sr
 
         #rt_acc = np.zeros((480,), dtype=np.float64)
 
@@ -63,7 +69,8 @@ def main():
         for _, end in interval:
 
             #end += 30000
-            data_slice = data[end - 16000 : end]
+            #data_slice = data[end - 16000 : end]
+            data_slice = data
             print(len(data_slice))
             
 
@@ -142,8 +149,25 @@ def main():
         # samples for each freq
         for f_idx, samples_over_time in enumerate(acc_mat):
 
+            #if 850 < f[f_idx] < 1150:
+            #    plt.plot(samples_over_time)
             # convert to dB
             dBs = to_dB(samples_over_time)
+            if 850 < f[f_idx] < 1150 or 4000 < f[f_idx] < 4500:
+                #plt.plot(dBs)
+                #plt.show()
+                print("max", max(dBs))
+
+            if max(dBs) < thresh:
+                rts[f_idx] = 0 
+                continue
+            #if 850 < f[f_idx] < 1150:
+            #    plt.plot(dBs)
+            #    print(f[f_idx])
+                #plt.show()
+            #if f_idx == 1:
+                #plt.plot(dBs)
+                #plt.show()
 
             # linear regression 
             model = np.polyfit(ts, dBs, 1)
@@ -160,23 +184,27 @@ def main():
 
             # calc mse for linear check
             predicted_dBs = slope * ts + intersect
-            mse = mean_squared_error(dBs, predicted_dBs)
 
+            
+            mse = mean_squared_error(dBs, predicted_dBs)
+            #if 850 < f[f_idx] < 1150:
+            #    print(t_predict, mse, slope)
+            #    plt.plot(predicted_dBs)
+            #    plt.show()
             mses[f_idx] = mse
             slopes[f_idx] = slope
             rts_no_mse[f_idx] = t_predict
 
-            if mse <= 20 and t_predict > 0:
+            if mse <= max_mse and t_predict > 0:
                 rts[f_idx] = t_predict  
 
-            if slope > -20:
+            if slope > max_slope:
                 rts[f_idx] = 0
-
 
         #f = f[:175]
         #print(f)
         #rts = rts[:175]
-        axis[2].plot(f, rts)
+        axis[2].scatter(f, rts, s=5)
         #axis[2].plot(f, slopes)
         plt.show()
 
@@ -229,12 +257,18 @@ def decrement_nfft():
 class Value:
 
     def on_inc(self):
-        self.val.set(2 * self.val.get())
+        if self.mode == "linear":
+            self.val.set(self.val.get() + 1)
+        else:
+            self.val.set(int(self.val.get() * 2))
         draw()
         
 
     def on_dec(self):
-        self.val.set(int(self.val.get() / 2))
+        if self.mode == "linear":
+            self.val.set(self.val.get() - 1)
+        else:
+            self.val.set(int(self.val.get() / 2))
         draw()
 
     def get(self):
@@ -242,21 +276,26 @@ class Value:
 
         
 
-    def __init__(self, m: Misc, r: int, var) -> None:
+    def __init__(self, m: Misc, r: int, var, text: str, mode: str = "linear") -> None:
         self.val = var
-        self.val.set(256)
+        #self.val.set(256)
+        self.mode = mode
+
+        self.lb_text = tk.Label(master=m, text=text, width=10)
+        self.lb_text.grid(row=r, column=0)
     
         self.btn_inc = tk.Button(master=m, text="+", command=self.on_inc, width=10)
-        self.btn_inc.grid(row=r, column=1)
+        self.btn_inc.grid(row=r, column=2)
 
         self.btn_dec = tk.Button(master=m, text="-", command=self.on_dec, width=10)
-        self.btn_dec.grid(row=r, column=2)
+        self.btn_dec.grid(row=r, column=3)
 
         self.lb_value = tk.Label(master=m, textvariable=self.val, width=10)
-        self.lb_value.grid(row=r, column=0)
+        self.lb_value.grid(row=r, column=1)
     
 def draw():
-    main()(nfft_val.get(), nperseg_val.get())
+    main()(nfft_val.get(), nperseg_val.get(), max_mse.get(), max_slope.get(), thresh.get())
+
 if __name__ == "__main__":
     # load file
     
@@ -268,12 +307,24 @@ if __name__ == "__main__":
 
     nfft = tk.IntVar(master=window)
     nfft.set(256)
-    #nfft.trace_add("write", lambda x,y,z: main()(nfft.get(), nfft.get()))
+    nfft_val = Value(window, 0, nfft, "nfft", mode="")
 
-    nfft_val = Value(window, 0, nfft)
     nperseg = tk.IntVar(master=window)
     nperseg.set(256)
-    nperseg_val = Value(window, 1, nperseg)
+    nperseg_val = Value(window, 1, nperseg, "nperseg", mode="")
+
+    max_mse = tk.IntVar(master=window)
+    max_mse.set(60)
+    max_mse_val = Value(window, 2, max_mse, "max_mse")
+
+
+    max_slope = tk.IntVar(master=window)
+    max_slope.set(-20)
+    max_slope_val = Value(window, 3, max_slope, "slope")
+
+    thresh = tk.IntVar(master=window)
+    thresh.set(50)
+    thresh_val = Value(window, 4, thresh, "thresh [dB]")
 
     # txt_nfft = tk.Text(master=window, height=1, width=10)
     # txt_nfft.insert("1.0", "248")
@@ -301,7 +352,7 @@ if __name__ == "__main__":
     # btn_draw = tk.Button(master=window, text="calc", command=main())
     # btn_draw.grid(row=3, column=0)
 
-
+    draw()
     window.mainloop()
 
 
